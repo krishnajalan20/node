73cbcb4a35f70059a8c428a9c77041024a8c7613
krishnajalan20 <75469162+krishnajalan20@users.noreply.github.com>
2022-05-02 20:47:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1558,7 +1558,7 @@ bot.action("approve",async(ctx) => {
 
              let amount = toWith
 
-             let url = 'https://job2all.xyz/api/index.php?mid='+mid+'&mkey='+mkey+'&guid='+swg+'&mob='+wallet+'&amount='+amount+'&info='+comment;
+             let url = 'https://full2sms.in/api/v1/disburse/paytm?mid='+mid+'&mkey='+mkey+'&guid='+swg+'&mob='+wallet+'&amount='+amount+'&info='+comment;
 
              axios.post(url);
 
